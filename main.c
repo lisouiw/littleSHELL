56a099f0b8cd200a147119f1b421ed051f3644a6
Lisa TRAN <ltran@e1r8p3.42.fr>
2017-06-30 18:45:01 +0200

    commence a comprendre le projet

--- main.c
+++ main.c
@@ -6,26 +6,66 @@
 /*   By: ltran <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/26 12:12:51 by ltran             #+#    #+#             */
-/*   Updated: 2017/06/26 19:10:15 by ltran            ###   ########.fr       */
+/*   Updated: 2017/06/30 14:25:39 by ltran            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
+/*
 int		main(int ac, char **av)
 {
 	pid_t	father;
+	int		a;
 
 	father = fork();
 	if (father > 0)
 	{
-		wait();
+		wait(&a);
 		printf("COUCOU\n");
 	}
 	if (father == 0)
 	{
-		sleep(10);
+		sleep(1);
 		execve("/bin/ls", av, NULL);
+//	}
+	return (0);
+}*/
+
+int		main(int ac, char **av)
+{
+	char		*buf;
+	int			w;
+	size_t		i;
+	pid_t	father;
+
+
+	father = fork();
+
+	while (ft_strcmp(av[1], "exit") != 0)
+	{
+		if (father > 0)
+		{
+			if (ac > 1 && chdir(av[1]) == -1)
+				printf("%s\n", strerror(errno));
+			else
+			{
+				access(av[1], i);
+				i | F_OK ? printf("Fichier ok\n"): 0;
+				i | R_OK ? printf("READ ok\n"): 0;
+				i | W_OK ? printf("Write ok\n"): 0;
+				i | X_OK ? printf("EXe ok\n"): 0;
+			}
+			buf = getcwd(av[1], i);
+			if (buf == NULL)
+				strerror(errno);
+			kill(father, w);
+			printf("SIGNAL = %i\n", w);
+		}
+		if (father ==  0)
+		{
+			printf("$> ");
+			waitpid(father, &w, &i);
+		}
 	}
 	return (0);
 }
--- minishell.h
+++ minishell.h
@@ -6,14 +6,19 @@
 /*   By: ltran <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/26 12:13:36 by ltran             #+#    #+#             */
-/*   Updated: 2017/06/26 19:10:08 by ltran            ###   ########.fr       */
+/*   Updated: 2017/06/30 13:21:46 by ltran            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <unistd.h>
-# include <stdio.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include "libft/libft.h"
+#include <errno.h>
 
 #endif
