57a7687a57cefd80da45a079de19ee8e3e4a390e
Lisa TRAN <ltran@e1r11p15.42.fr>
2017-08-23 21:36:41 +0200

    quote export

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ltran <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/26 12:12:51 by ltran             #+#    #+#             */
-/*   Updated: 2017/08/22 18:07:15 by ltran            ###   ########.fr       */
+/*   Updated: 2017/08/23 21:36:27 by ltran            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,10 +81,6 @@ char	*line_split(char *len, char *str)
 	return (NULL);
 }
 
-void	b_export()
-{
-	//export NAME=name NAME=name
-}
 
 void	b_cd(char **way, char *home, char *pwd)
 {
@@ -159,66 +155,189 @@ void	my_strjoin(char **spc, char **line, int a)
 		else
 			tmp = ft_strjoin(line[a], " ");
 		*spc = ft_strjoin(tmp, line[a + 1]);
-	//	ft_putendl(*spc);
 		++a;
 	}
 }
 
-
-
-void	b_echo(char **echo, int a)
+size_t	all_d_len(char **echo, int a)
 {
-	while (echo[a])
+	int			o;
+	size_t		len;
+
+	while (echo[++a])
 	{
-		ft_putstr(echo[a]);
-		if (echo[++a])
-			ft_putchar(' ');
+		o = -1;
+		while (echo[a][++o])
+			;
+		len += o;
 	}
-	write(1, "\n", 1);
+	len = len + a + 1;
+	return (len);
 }
 
-void	exec_cmd(char **line)
+
+int		b_echo(char **echo, int a, int o, char *join)
 {
-	char	*spc;
+	int			i;
+	int			y;
+	char		*line;
+	char		*join2;
+	char		**cut;
+
+	y = 0;
+	join2 = join;
+	if(!(join = (char*)malloc(all_d_len(echo, a+1)*sizeof(char))))
+		return(-1);
+	while (echo[++a])
+	{
+		i = -1;
+		if (y != 0)
+			join[y++] = ' ';
+		while (echo[a][++i])
+		{
+			if (echo[a][i] != '"' && echo[a][i] != 39)
+				join[y++] = echo[a][i];
+			else if (echo[a][i] == 39 && o < 2)
+				o = (o == 0) ? 1 : 0;
+			else if (echo[a][i] == '"' && (o == 0 || o == 2))
+				o = (o == 0) ? 2 : 0;
+		}
+	}
+	join[y] = '\n';
+	join[y + 1] = '\0';
+	if (join2 != NULL)
+		join = ft_strjoin(join2, join);
+	if (o == 0)
+		ft_putstr(join);
+	while (o != 0)
+	{
+		o == 1 ? ft_putstr("quote> ") : ft_putstr("dquote> ");
+		get_next_line(0, &line);
+		cut = strsplit_two_c(line, '\t', ' ');
+		o = b_echo(cut, -1, o, join);
+	}
+	return (o);
+}
+
+void	b_export(char **cut, t_env *env)
+{
+	int		i;
+	t_env	*tmp;
+	t_env	*kp;
+
+	i = 0;
+	while (cut[++i])
+	{
+		kp = env;
+		if ((ft_strchr(cut[i], '=')))
+		{
+			tmp = (t_env*)malloc(sizeof(t_env));
+			tmp = NULL;
+			tmp = add_env(cut[i], tmp, ft_strlen(ft_strchr(cut[i], '=')),
+				ft_strlen(cut[i]));
+			while (kp->next != NULL && ft_strcmp(kp->name, tmp->name) != 0)
+				kp = kp->next;
+			if (ft_strcmp(kp->name, tmp->name) == 0)
+			{
+				free(kp->ctn);
+				kp->ctn = ft_strdup(tmp->ctn);
+				free_list(tmp);
+			}
+			else if (kp->next == NULL)
+				env = add_env(cut[i], env, ft_strlen(ft_strchr(cut[i], '=')),
+					ft_strlen(cut[i]));
+		}
+	}
+}
+
+void	b_unset(char **cut, t_env **env)
+{
+	int		i;
+	char	*nm;
+
+	i = 0;
+	while (cut[++i])
+	{
+		//61
+	}
+	return;
+}
+
+void	exec_cmd(char *line, t_env *env)
+{
 	char	**cut;
 
-	spc = NULL;
-	my_strjoin(&spc, line, 0);
-	if(!(cut = strsplit_two_c(spc, '\t', ' ')) || !cut[0])
+	if (!(cut = strsplit_two_c(line, '\t', ' ')) || !cut[0])
 		return;
 	if (ft_strcmp("echo", cut[0]) == 0)
-		b_echo(cut, 1);
-	if (ft_strcmp("env", cut[0]) == 0)
-		;
-	if (ft_strcmp("export", cut[0]) == 0)
-		;
-	if (ft_strcmp("unsetenv", cut[0]) == 0)
-		;
-	if (ft_strcmp("cd", cut[0]) == 0)
+	{
+		if (cut[1])
+			b_echo(cut, 0, 0, NULL);
+		else
+			write(1, "\n", 1);
+	}
+	else if (ft_strcmp("env", cut[0]) == 0)
+		ecriture_info(env);
+	else if (ft_strcmp("export", cut[0]) == 0 && cut[1])
+		b_export(cut, env);
+	else if (ft_strcmp("unset", cut[0]) == 0)
+		b_unset(cut, &env);
+	else if (ft_strcmp("cd", cut[0]) == 0)
 		;
 	else
 		;
 }
 
+t_env	*add_env(char *environ, t_env *env, size_t one, size_t all)
+{
+	t_env	*new;
+	t_env	*tmp;
+
+	tmp = env;
+	new = (t_env*)malloc(sizeof(t_env));
+	new->name = ft_strsub(environ, 0, all - one + 1);
+	new->ctn = ft_strsub(environ, all - one + 1, one -1);
+	new->next = NULL;
+	if (env == NULL)
+	{
+		new->next = env;
+		return (new);
+	}
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	tmp->next = new;
+	return (env);
+}
+
+t_env	*give_env(t_env *env)
+{
+	int				i;
+	extern char		**environ;
+
+	env = (t_env*)malloc(sizeof(t_env));
+	env = NULL;
+	i = -1;
+	while (environ[++i])
+	{
+		env = add_env(environ[i], env, ft_strlen(ft_strchr(environ[i], '=')),
+				ft_strlen(environ[i]));
+	}
+	return (env);
+}
+
 int		main(int argc, char **argv, char **env)
 {
 	char	*line;
-	int		i;
-	int		w;
-	char	*cm;
-	pid_t	father;
-	char	**path = NULL;
-	char	*home;
-	char	*pwd;
+	t_env	*envs;
 
-	//env recup avec liste chainee
+	envs = give_env(NULL);
 	while (42)
 	{
 		ft_putstr("(.Y.)> ");
 		if (get_next_line(0, &line) && ft_strcmp(line, "exit") == 0)
 			exit(0);
 		else
-			exec_cmd(/*argv = i*/ft_strsplit(line, ' '));
+			exec_cmd(line, envs);
 	}
 	return (0);
 }
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: ltran <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/26 12:13:36 by ltran             #+#    #+#             */
-/*   Updated: 2017/08/22 16:22:30 by ltran            ###   ########.fr       */
+/*   Updated: 2017/08/23 19:10:09 by ltran            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,18 @@
 # include <sys/types.h>
 # include <dirent.h>
 
+typedef struct			s_env
+{
+	char				*name;
+	char				*ctn;
+	struct s_env		*next;
+}						t_env;
+
+
+void	free_list(t_env *env);
+t_env	*add_env(char *environ, t_env *env, size_t one, size_t all);
+void	ecriture_info(t_env *lst);
+t_env	*give_env(t_env *env);
 char	**strsplit_two_c(char const *s, char c, char d);
 
 #endif
